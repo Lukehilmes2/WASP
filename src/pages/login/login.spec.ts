ef5d55cb7856ce8c1116accbc4d81c07942e5d10
Lukehilmes2 <lukehilmes2@gmail.com>
2018-11-19 11:57:23 -0700

    Correct package.json and package-lock.json updated README for installing and testing and formatted login.spec so npm test will run

--- src/pages/login/login.spec.ts
+++ src/pages/login/login.spec.ts
@@ -2,9 +2,17 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By }           from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 import { LoginPage } from './login';
-import { IonicModule, Platform, NavController} from 'ionic-angular/index';
+import { IonicModule, Platform, NavController, NavParams,LoadingController, Loading,AlertController, ModalController} from 'ionic-angular/index';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthProvider } from '../../providers/auth/auth';
+import { EmailValidator } from '../../validators/email';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpModule } from '@angular/http';
+import { AngularFireAuth } from 'angularfire2/auth';
+import {BehaviorSubject} from 'rxjs';
 
 describe('Login', () => {
   let component: LoginPage;
@@ -17,19 +25,39 @@ describe('Login', () => {
         IonicModule.forRoot(LoginPage)
       ],
       providers: [
-        NavController
+        IonicModule,
+        NavController,
+        {provide: NavParams, useClass: MockNavParams},
+        LoadingController,
+        AlertController,
+        ModalController,
+        AuthProvider,
+        {provide:AngularFireAuth, useValue: AngularFireAuthStub}
       ]
     });
-  }));
-
-  beforeEach(() => {
     fixture = TestBed.createComponent(LoginPage);
     component = fixture.componentInstance;
-   
-  });
+  }));
 
-  it('should be true', () => 
-  expect(true).toBe(true)
-  );
+
+  it('should create component', () => expect(component).toBeDefined());
+
 
 });
+
+class MockNavParams{
+  data = {
+  };
+
+  get(param){
+    return this.data[param];
+  }
+}
+const AngularFireAuthStub = {
+  collection: (name: string) => ({
+    doc: (_id: string) => ({
+      valueChanges: () => new BehaviorSubject({ foo: 'bar' }),
+      set: (_d: any) => new Promise((resolve, _reject) => resolve()),
+    }),
+  }),
+};
